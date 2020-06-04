d72ad9b4de705aaecbe6ea8905866ea012cf2798
Thom May <thom@may.lt>
2020-06-04 11:32:19 +0100

    Be a bit more robust
    Now we'll just move on in the face of bad requests, rather than blowing up.
    Also, rustfmt.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
-use serde::Deserialize;
-use std::path::{PathBuf};
-use std::fs::{OpenOptions};
-use std::io::{Read, Write};
 use anyhow::Result;
+use serde::Deserialize;
+use std::fs::OpenOptions;
+use std::io::{Read, Write};
+use std::path::PathBuf;
 
-fn main() -> Result<()>{
+fn main() -> Result<()> {
     let c = std::env::args().last().unwrap();
     let mut cf = OpenOptions::new().read(true).open(c)?;
     let mut cfg = String::new();
     cf.read_to_string(&mut cfg)?;
     let config: Config = toml::from_str(&cfg)?;
-    let mut urls = vec!();
+    let mut urls = vec![];
     for url in config.blacklists {
-        let body: String = reqwest::blocking::get(&url)?.text()?;
+        let body: String = if let Ok(req) = reqwest::blocking::get(&url) {
+            req.text()?
+        } else {
+            dbg!(format!("got busted URL: {}", url));
+            continue;
+        };
         for line in body.lines() {
-            if line.is_empty() || line.starts_with('#') { continue; }
+            if line.is_empty() || line.starts_with('#') {
+                continue;
+            }
             let line = line.split('#').collect::<Vec<&str>>();
             let line = line.first().unwrap().trim();
             let line = String::from(line.trim_end_matches('\n'));
@@ -29,12 +36,16 @@ fn main() -> Result<()>{
     }
     let mut output = String::from(output.trim_end_matches(','));
     output.push('}');
-    let mut o = OpenOptions::new().create(true).truncate(true).write(true).open(config.output)?;
+    let mut o = OpenOptions::new()
+        .create(true)
+        .truncate(true)
+        .write(true)
+        .open(config.output)?;
     o.write(output.as_bytes())?;
     Ok(())
 }
 
-#[derive(Default,Debug,Clone,Deserialize)]
+#[derive(Default, Debug, Clone, Deserialize)]
 struct Config {
     blacklists: Vec<String>,
     output: PathBuf,
